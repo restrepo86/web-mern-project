f9cb450325ecee7cd24a0d83cc1e984212a4dcec
Juan Fernando Restrepo <JuanRestrepo>
2020-04-07 17:03:44 -0500

    Se agregan configuraciones para que por medio de reac-router-dom se rendericen automaticamente  los componentes, al agregarle la configuración en el archivo routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import routes from './config/routes';
 
 import './App.scss';
-import Admin from './pages/Admin';
-import SignIn from './pages/Admin/SignIn';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
+
 
 function App() {
 
   return (
-    <>
-      <h1>Estamos en App.js</h1>
-      <Admin />
-      <SignIn />
-      <Home />
-      <Contact />
-    </>
+    <Router>
+      <Switch>
+        {routes.map((route, index) => (
+          <RouteWithSubRoutes key={index} {...route} />
+        ))}
+      </Switch>
+    </Router>
   );
-}
+};
+
+function RouteWithSubRoutes(route) {
+  console.log(route);
+  return (
+    <Route
+      path={route.path}
+      exact={route.exact}
+      render={props => <route.component routes={ route.routes } { ...props } />}
+    />
+  );
+};
 
 
 export default App;
