14e29e8837fd56a909e4dec02adeb96402f64a7e
Juan Fernando Restrepo <JuanRestrepo>
2020-04-07 11:52:50 -0500

    Se agrega librería antd design al proyecto para obtener componentes graficos para la app web

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { DatePicker } from 'antd';
+
 import './App.scss';
 
 function App() {
+
+  const test = (date, dateString) => {
+    console.log(date, dateString);
+    
+  };
+
   return (
-    <div className="app">
+    <div className=",app">
       <h1>Hola App React</h1>
       <h2>Proyecto</h2>
+      <DatePicker onChange={test}/>
     </div>
   );
 }
