507be3ae233b0491a94e3450ee8cc769fa0aff6b
Juan Fernando Restrepo <JuanRestrepo>
2020-04-07 14:18:35 -0500

    Se agrega implementacion de react-router para renderizar diferentes componentes en el DOM virtual y si no existe la ruta renderizar un componente 404

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { DatePicker } from 'antd';
 
 import './App.scss';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
 
-  const test = (date, dateString) => {
-    console.log(date, dateString);
-    
-  };
 
   return (
-    <div className=",app">
-      <h1>Hola App React</h1>
-      <h2>Proyecto</h2>
-      <DatePicker onChange={test}/>
-    </div>
+    <Router>
+      <div className=",app">
+        <h1>Sistema de Rutas Basico</h1>
+
+        <Link to='/'>Home</Link> <br/>
+        <Link to='/contact'>Contact</Link> <br/>
+        <Link to='/users'>Users</Link> <br/>
+
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route exact path='/contact' component={ Contact } />
+          <Route exact path='/users' component={ Users } />
+          <Route component={ Error404 } />
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
+
+function Home() {
+  return <h2>Estamos en el componente home ...</h2>
+};
+
+function Contact() {
+  return <h2>Estamos en el componente Contact ...</h2>
+};
+
+function Users() {
+  return <h2>Estamos en el componente Users ...</h2>
+};
+
+function Error404() {
+  return <h2>Error 404</h2>
+};
 
 export default App;
