a4a70f8d3ec1f04207d53d6e2e9cdae7eb0d6b77
Juan Fernando Restrepo <JuanRestrepo>
2020-04-07 15:20:51 -0500

    Se crean paginas de la aplicacion

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,23 @@
 import React from 'react';
 
 import './App.scss';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Admin from './pages/Admin';
+import SignIn from './pages/Admin/SignIn';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
 
-
   return (
-    <Router>
-      <div className=",app">
-        <h1>Sistema de Rutas Basico</h1>
-
-        <Link to='/'>Home</Link> <br/>
-        <Link to='/contact'>Contact</Link> <br/>
-        <Link to='/users'>Users</Link> <br/>
-
-        <Switch>
-          <Route exact path='/' component={ Home } />
-          <Route exact path='/contact' component={ Contact } />
-          <Route exact path='/users' component={ Users } />
-          <Route component={ Error404 } />
-        </Switch>
-        
-      </div>
-    </Router>
+    <>
+      <h1>Estamos en App.js</h1>
+      <Admin />
+      <SignIn />
+      <Home />
+      <Contact />
+    </>
   );
 }
 
-function Home() {
-  return <h2>Estamos en el componente home ...</h2>
-};
-
-function Contact() {
-  return <h2>Estamos en el componente Contact ...</h2>
-};
-
-function Users() {
-  return <h2>Estamos en el componente Users ...</h2>
-};
-
-function Error404() {
-  return <h2>Error 404</h2>
-};
 
 export default App;
