0276859995c098b799f0ce97d1310fe2cceeea31
Juan Fernando Restrepo <JuanRestrepo>
2020-04-09 12:23:32 -0500

    se crea implementación en el SERVER node crear un endpoint post REST

--- server/app.js
+++ server/app.js
@@ -5,8 +5,13 @@ const app = express();
 const { API_VERSION } = require('./config');
 
 
+const userRoutes = require('./routers/user');
+
+
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
+
+app.use(`/api/${API_VERSION}`, userRoutes);
 
 module.exports = app;
